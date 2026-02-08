da55b7fa18c9ed726e3ab9a8d95fdc1093d9a21d
Zweena Ariva <ariva02zweena@gmail.com>
2026-02-08 13:42:02 +0700

    first commit

---
+++ JavaApplication/Day2.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.awt.Point;
+
+public class Day2 {
+    public static void main(String[] args) {
+        // byte tanggal = 4;
+        // Date besok = new Date();  // Membuat objek Date baru untuk tanggal dan waktu saat ini
+        // System.out.println(besok);
+
+        // byte x = 5; //ini tentang primitive type, jadi dia menyimpan nilai langsung
+        // byte y = x;  // Copy by value
+        // x = 2;
+        // System.out.println(y); //Jadi ketika x diubah, y tidak berubah karena mereka menyimpan nilai yang berbeda
+
+        // Point point1 = new Point(1, 1); //ini tentang reference type, jadi dia menunjuk ke alamat memory
+        // Point point2 = point1;  //Pointer copy by reference
+        // point1.x = 2;  //Jadi ketika point1 diubah, point2 juga ikut berubah karena keduanya merujuk ke objek yang sama
+        // System.out.println(point2);
+
+
+    }
+}
---
+++ JavaApplication/JavaApplication3.java
@@ -0,0 +1,13 @@
+package javaapplication3;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        byte age = 20;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+    }
+
+}
