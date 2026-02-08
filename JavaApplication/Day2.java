6c6e2e3ffba857037e9a6462b3b085829c18d8cb
Zweena Ariva <ariva02zweena@gmail.com>
2026-02-08 14:11:07 +0700

    about string

--- JavaApplication/Day2.java
+++ JavaApplication/Day2.java
@@ -17,6 +17,18 @@ public class Day2 {
         // point1.x = 2;  //Jadi ketika point1 diubah, point2 juga ikut berubah karena keduanya merujuk ke objek yang sama
         // System.out.println(point2);
 
+        // String message = "Hello World" + "!!";
+        // System.out.println(message);
 
+        // String message = "     Hello World" + "!!"; //contoh method pada string
+        // System.out.println(message.endsWith("!!")); //cek apakah string diakhiri dengan "!!"
+        // System.out.println(message.startsWith("!!")); //cek apakah string diawali dengan "!!"
+        // System.out.println(message.length()); //menghitung panjang string
+        // System.out.println(message.indexOf("x")); //mencari posisi karakter 'x' dalam string
+        // System.out.println(message.replace("World", "Java")); //mengganti "World" dengan "Java"
+        // System.out.println(message.toLowerCase()); //mengubah string menjadi huruf kecil
+        // System.out.println(message.toUpperCase()); //mengubah string menjadi huruf besar
+        // System.out.println(message.trim()); //menghapus spasi di awal dan akhir string
+        // System.out.println(message); //string itu immutable, jadi nilai aslinya tidak berubah
     }
 }
